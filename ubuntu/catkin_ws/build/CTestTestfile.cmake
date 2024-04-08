2c8527df2b68e89cf295857dc3fe3e81396c38d9
shahrammohsini <43254791+shahrammohsini@users.noreply.github.com>
2024-04-08 14:52:17 -0400

    created the catkin workspace

---
+++ ubuntu/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/shahram/Documents/GitHub/Masters/ubuntu/catkin_ws/src
+# Build directory: /home/shahram/Documents/GitHub/Masters/ubuntu/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("my_robot_controller")
---
+++ ubuntu/catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/shahram/Documents/GitHub/Masters/ubuntu/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/shahram/Documents/GitHub/Masters/ubuntu/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/shahram/Documents/GitHub/Masters/ubuntu/catkin_ws/devel;/opt/ros/noetic' != '' else []
